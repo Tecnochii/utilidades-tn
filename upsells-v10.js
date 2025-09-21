024b53a7be6f036ff5598d7480a098d3a6c7bcda
Emmanuel Leiva <ema62x@gmail.com>
2025-09-21 01:22:09 -0300

    texto

--- upsells-v10.js
+++ upsells-v10.js
@@ -78,7 +78,7 @@ const loadUpsellingProducts = () => {
 <div style="margin-left: 10px; border: 1px solid green;" class="discount">${product.discount}</div>
 </div>
 
-${dataEnvioText ? ` <div display="flex">
+${dataEnvioText ? ` <div style="display: flex;">
   <div style="color:green;" class="envio-text">${dataEnvioText}</div>
   <div></div> 
   </div>  ` : ""}
